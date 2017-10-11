ee4d912cb194bb02723b0de9377d60c4dc3b6333
Andrea <andreamiralles686@gmail.com>
2017-10-11 16:19:42 -0700

    parse tweets to add to db, find search results and send to client

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const Tweet = require('./../db/index');
+const parseTweets = require('./utils/tweetConstructor');
 const app = express();
 
 app.use(express.static(__dirname + '/../client/dist/'));
@@ -7,6 +8,29 @@ app.use(express.static(__dirname + '/../client/dist/'));
 app.get('/', (req, res) => {
   res.send('hello world!');
 });
+
+app.get('/tweets', (req, res) => {
+  console.log('+++++++GET REQ', req.query.query);
+  // Tweet.find({$regex : `.*${req.query.query}.*`})
+
+  // $or
+  console.log('^^^^^^^', typeof req.query.query)
+
+  Tweet.find({$text: { $search: req.query.query}}, (err, matches) => {
+    console.log('{{{{{{{{{ match', matches); //array
+    res.json(matches);
+
+  });
+  // if (err) {
+  //   res.sendStatus(500);
+  // } else {
+  //   res.send(req.query.query);
+  // }
+});
+
+// uncomment to add tweets to db
+// parseTweets();
+
 
 var port = 3000;
 
--- server/utils/tweetConstructor.js
+++ server/utils/tweetConstructor.js
@@ -1,60 +1,78 @@
+const Promise = require('bluebird');
 const fs = require('fs');
+const Tweet = require('../../db/index');
+const getDate = require('./dateTimeHelper');
 
-const colors = {
-  A: 'red',
-  B: 'orange',
-  C: 'yellow',
-  D: 'olive',
-  E: 'green',
-  F: 'teal',
-  G: 'blue',
-  H: 'violet',
-  I: 'purple',
-  J: 'pink',
-  K: 'brown',
-  L: 'grey',
-  M: 'black',
-  N: 'red',
-  O: 'orange',
-  P: 'yellow',
-  Q: 'olive',
-  R: 'green',
-  S: 'teal',
-  T: 'blue',
-  U: 'violet',
-  V: 'purple',
-  W: 'pink',
-  X: 'brown',
-  Y: 'grey',
-  Z: 'black',
-}; 
+const colors = ['red', 'orange', 'yellow', 'olive', 'green', 'teal', 'blue', 'violet', 'purple', 'pink', 'brown', 'grey', 'black'];
 
-const splitTweet = (err, data) => {
-  if (err) { console.log('ERROR', err); }
-  data = data.toString().split('\n');
-  data.forEach(tweet => {
 
-    tweet = tweet.replace(/\s\s+/g, ' ');
-    console.log(tweet);
-    
-  });
-  // console.log(data);
+const createDBTweetObj = (tweets) => {
+  console.log('initiate create document');
+  Tweet.remove().then(() => {
+    tweets.forEach(el => {
+      var tweet = new Tweet({
+        username: el[el.length - 1],
+        dateTime: getDate(el.slice(0, 2).join(' ')),
+        text: el.slice(2, -1).join(' '),
+        color: colors[Math.floor(Math.random() * 12)]
+      });
+      tweet.save((err, tweet) => { if (err) { console.error(err); } });
+    });
+  }).catch(err => { console.log('ERR removing existing tweet document/adding tweets to db', err); });
 };
 
-const getUsername = (tweet) => {
-  let words = tweet.split(' ');
-  let username = words[words.length - 1];
-  return username;
+// const getUsername = (tweet) => {
+//   let username = tweet[tweet.length - 1];
+//   return username;
+// };
+
+const modifyHaiku = (tweets, cb) => {
+  // pop last el, which is an empty arr
+  tweets.pop();
+  for (var i = 0; i < tweets.length; i++) {
+    if (tweets[i][0].slice(0, 4) !== '2014') {
+      var start = i - 1;
+      counter = i;
+      while (tweets[counter][0].slice(0, 4) !== '2014') {
+        tweets[start] = tweets[start].concat(tweets[counter]);
+        tweets[start].push('\n');
+        tweets.splice(counter, 1);
+      }
+      tweets[start].pop();
+    }
+  }
+  // simple check that all usernames (i.e. accounts) are nytimes (in place of tests)
+  // var usernames = tweets.map(el => el[el.length - 1]);
+  // var notNYT = usernames.filter(el => el !== 'nytimes');
+  // console.log(notNYT.length); //0
+
+  // simple check for concatenated tweetArray for haiku tweets (in place of tests)
+  // var fixed = tweets.map(el => el[0]);
+  // var notDate = tweets.filter(el => el[0].slice(0, 4) !== '2014');
+  // console.log(notDate.length) //0
+
+  createDBTweetObj(tweets);
 };
 
-const createTweets = () => {
-  // create id in server
-  // parse msg
-  fs.readFile('../../assignment_tweet.txt', 'utf8', splitTweet);
+const splitWords = (data, cb) => {
+  // remove extra spaces in each tweet, split by word, remove trailing space
+  console.log(data)
+  let parsedTweet = data.map(tweet => tweet.replace(/\s\s+/g, ' ').split(' ').filter(el => el.length > 0));
+  cb(parsedTweet, createDBTweetObj);
 };
 
-createTweets();
-
-module.exports.createDBTweetObj = () => {
-
+const splitTweets = (err, data) => {
+  // create 1 arr for each tweet
+  if (err) { console.log('ERROR splitting tweets', err); }
+  let input = data.toString().split('\n'); 
+  console.log(input)
+  splitWords(input, modifyHaiku);
 };
+
+
+const parseTweets = (cb) => {
+  fs.readFile(__dirname + '/../../assignment_tweet.txt', 'utf8', splitTweets);
+};
+
+module.exports = parseTweets;
+
